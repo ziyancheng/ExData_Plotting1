e09dbd7f70e5d5f171ce98e681c741aee182dbe8
ziyancheng <ziyancheng@gmail.com>
2018-09-07 16:52:55 -0400

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 # Exploratory Data and Analysis Project1
-
+# the datafile should be in the current working dirtory
 # read and only focus on the data from 1/2/2007-2/2/2007
 
 datafile <- read.table("household_power_consumption.txt", skip = 66637, nrows = 2880,sep = ";") 
@@ -10,3 +10,6 @@ subsetData <- datafile %>% select(V3)  %>% mutate(V3 = as.numeric(as.character(V
 
 
 hist(datafile$V3, col = "red", main = "Global Active Power", xlab = "Global Active Power (killowatts)", ylab = "Frequency")
+png("plot1.png")
+dev.off()
+
