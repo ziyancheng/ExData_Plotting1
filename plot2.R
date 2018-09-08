766102a178da09d2c9430a409862f841ea341deb
ziyancheng <ziyancheng@gmail.com>
2018-09-08 11:51:04 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,6 +4,8 @@ data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, n
 # clearing name row and unrelevant variables, casting to right classes
 data <- data %>% mutate(V1 = as.POSIXct(dmy_hms(as.character(paste(V1, V2)))),
                         V3 = as.numeric(as.character(V3))) %>% select(V1,V3)
-png("plot2.png", width=480, height=480)
+
 # create plot using with
 with(data, plot(V1,V3, type="l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+dev.copy(png, file ="plot2.png", width = 480, height = 480)
+dev.off()
